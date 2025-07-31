781004c03a3a59a471785f47fbe97bd665374998
LeChen <lecself@163.com>
2025-07-31 18:40:29 +0800

    feat: support eagle for qwen2/qwen3

--- vllm/config.py
+++ vllm/config.py
@@ -2904,7 +2904,7 @@ class SpeculativeConfig:
                              f"{self.disable_by_batch_size=}")
 
         if self.method == "eagle3" and self.target_model_config and \
-            "llama" not in self.target_model_config.hf_text_config.model_type:
+            not any(x in self.target_model_config.hf_text_config.model_type for x in ["llama", "qwen"]):
             raise ValueError(
                 "Eagle3 is only supported for Llama models. "
                 f"Got {self.target_model_config.hf_text_config.model_type=}")
